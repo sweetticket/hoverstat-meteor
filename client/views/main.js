c67aef7ace18d9a877c17d1e93af33d6e1af0a23
Jenny Kim <yk494@cornell.edu>
2015-07-30 15:00:50 -0400

    products section

--- client/views/main.js
+++ client/views/main.js
@@ -50,9 +50,11 @@ if (Meteor.isClient) {
 
   }
 
+
   Template.Main.events({
 
   });
+
 
   Template.Products.events({
   //   'click button': function () {
@@ -60,51 +62,51 @@ if (Meteor.isClient) {
   //     Session.set('counter', Session.get('counter') + 1);
   //   }
 
-      'mouseover .product-img': function (event, template) {
+      // 'mouseover .product-img': function (event, template) {
 
-          var cad_text = "<h2>Aerial CADs Stat</h2><br>" +
-                    "<p class='flow-text'>HoverStat provides 3D Aerial CADs with measurements captured by a" +
-                      " drone on-site in under 10 minutes.<br><br><strong>Hover over the images above to view descriptions.</strong></p>";
-          var crm_text = "<h2>Integrated CRM Solution</h2><br>" +
-                      "<p class='flow-text'>An integrated solution that allows contractors to collect leads," +
-                      " inspect the roof, and produce an estimate, all within one moblie app.<br><br><strong>Hover over the images above to view descriptions.</strong></p>";
-          var estimatestat_text = "<h2>EstimateStat</h2><br>" +
-                      "<p class='flow-text'>The estimator contains a rules engine that allows you to produce an" +
-                      " estimate with accurate calculations every time.<br><br><strong>Hover over the images above to view descriptions.</strong></p>";
-          var erp_text = "<h2>ERP: From Lead to Order</h2><br>" +
-                      "<p class='flow-text'>This mobile solution works to bring your business full cycle, from" +
-                      " the moment a canvasser knocks on the door to placing the purchase order.<br><br><strong>Hover over the images above to view descriptions.</strong></p>";
+      //     var cad_text = "<h2>Aerial CADs Stat</h2><br>" +
+      //               "<p class='flow-text'>HoverStat provides 3D Aerial CADs with measurements captured by a" +
+      //                 " drone on-site in under 10 minutes.<br><br><strong>Hover over the images above to view descriptions.</strong></p>";
+      //     var crm_text = "<h2>Integrated CRM Solution</h2><br>" +
+      //                 "<p class='flow-text'>An integrated solution that allows contractors to collect leads," +
+      //                 " inspect the roof, and produce an estimate, all within one moblie app.<br><br><strong>Hover over the images above to view descriptions.</strong></p>";
+      //     var estimatestat_text = "<h2>EstimateStat</h2><br>" +
+      //                 "<p class='flow-text'>The estimator contains a rules engine that allows you to produce an" +
+      //                 " estimate with accurate calculations every time.<br><br><strong>Hover over the images above to view descriptions.</strong></p>";
+      //     var erp_text = "<h2>ERP: From Lead to Order</h2><br>" +
+      //                 "<p class='flow-text'>This mobile solution works to bring your business full cycle, from" +
+      //                 " the moment a canvasser knocks on the door to placing the purchase order.<br><br><strong>Hover over the images above to view descriptions.</strong></p>";
 
-          var productID = $(event.target).attr('id');
+      //     var productID = $(event.target).attr('id');
 
-          $('#products-info').css('opacity', '0');
+      //     $('#products-info').css('opacity', '0');
 
-          switch (productID){
-            case "CAD":
-              $('#products-info').html(cad_text).animate({
-                  opacity: 1
-                }, 500);
-              break;
-            case "CRM":
-              $('#products-info').html(crm_text).animate({
-                  opacity: 1
-                }, 500);
-              break;
-            case "estimatestat":
-              $('#products-info').html(estimatestat_text).animate({
-                  opacity: 1
-                }, 500);
-              break;
-            case "ERP":
-              $('#products-info').html(erp_text).animate({
-                  opacity: 1
-                }, 500);
-              break;
-            default:
-              console.log("Mousehover did not match any product.")
-              break;
-          }
-      }
+      //     switch (productID){
+      //       case "CAD":
+      //         $('#products-info').html(cad_text).animate({
+      //             opacity: 1
+      //           }, 500);
+      //         break;
+      //       case "CRM":
+      //         $('#products-info').html(crm_text).animate({
+      //             opacity: 1
+      //           }, 500);
+      //         break;
+      //       case "estimatestat":
+      //         $('#products-info').html(estimatestat_text).animate({
+      //             opacity: 1
+      //           }, 500);
+      //         break;
+      //       case "ERP":
+      //         $('#products-info').html(erp_text).animate({
+      //             opacity: 1
+      //           }, 500);
+      //         break;
+      //       default:
+      //         console.log("Mousehover did not match any product.")
+      //         break;
+      //     }
+      // }
    });
 
   Template.Subscribe.rendered = function () {
