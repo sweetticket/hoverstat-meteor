5a787ea0eef6ad77cebf9972516f9197d3fcb03a
Jenny Kim <yk494@cornell.edu>
2015-08-06 11:04:01 -0400

    seo

--- app.js
+++ app.js
@@ -22,3 +22,24 @@
 //     Stripe.setPublishableKey('pk_test_8mXtjutkbZVxuyJnjxdiTWXF');
 //   });
 // }
+
+if (Meteor.isClient) {
+  Meteor.startup(function () {
+
+    document.title = "HoverStat";
+
+    return SEO.config({
+      title: 'HoverStat',
+      meta: {
+        'description': "With the press of a button, our drones inspect roofs and record measurements accurate down to 1.5cm in less than 10 minutes. Whether you need to look at the roof for solar, insurance, or roofing purposes, HoverStat is your one-stop solution for roof assessments."
+      },
+      og: {
+        'image': 'http://hoverstat.com/images/logo.png',
+        'title': 'HoverStat',
+        'type': 'website',
+        'url': 'http://hoverstat.com/',
+        'site_name': 'HoverStat'
+      }
+    });
+  });
+}
